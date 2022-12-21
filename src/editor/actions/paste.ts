6c13afb46b1795957ef3380c937dc85b2f6cc29d
Ali Chamas <ali.chamas@play.co>
2022-12-21 16:19:34 +1100

    cleanup

--- src/editor/actions/paste.ts
+++ src/editor/actions/paste.ts
@@ -26,35 +26,5 @@ export class PasteAction extends Action<void, void>
         app.undoStack.exec(new PasteCommand({
             nodeIds: clipboard.map((node) => node.id),
         }));
-
-        // const app = getApp();
-        // const selection = app.selection.hierarchy;
-        // const clipboard = app.getClipboard();
-
-        // if (clipboard.length === 1)
-        // {
-        //     const sourceNode = clipboard[0];
-        //     let newParentId = selection.length === 1
-        //         ? selection.firstNode.id
-        //         : sourceNode.getMetaNode().id;
-
-        //     if (app.selection.hierarchy.length === 1)
-        //     {
-        //         newParentId = app.selection.hierarchy.firstNode.id;
-        //     }
-
-        //     if (newParentId === sourceNode.id && sourceNode.parent)
-        //     {
-        //         newParentId = sourceNode.parent.id;
-        //     }
-
-        //     const { clonedNode } = app.undoStack.exec<CloneCommandReturn>(new CloneCommand({
-        //         nodeId: sourceNode.id,
-        //         cloneMode: CloneMode.Duplicate,
-        //         newParentId,
-        //     }));
-
-        //     selection.set(clonedNode);
-        // }
     }
 }
